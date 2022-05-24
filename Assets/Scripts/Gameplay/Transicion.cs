18938ef5d18ecb332faaf55b727c37c1ef326fb6
Rech99 <rech_99@hotmail.com>
2022-05-23 22:46:28 -0700

    transicion

---
+++ Assets/Scripts/Gameplay/Transicion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transicion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+ 
+}
+
